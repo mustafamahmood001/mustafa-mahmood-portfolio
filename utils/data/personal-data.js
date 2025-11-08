71041b6bdd0a68f23e601c6bc143e80d1d626075
Mustafa Mahmood <mustafamahmood001@outlook.com>
2025-11-08 11:08:42 +0500

    Fix

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "Mustafa Mahmood",
-  profile: '/cover-photo.JPG',
+profile: '/cover-photo.jpg',
   designation: "Software Engineer",
   description:" Hi, I’m Mustafa Mahmood — a passionate and professional Full Stack Software Developer. I’m a quick learner with a strong self-learning attitude and a deep interest in exploring new technologies.I enjoy solving complex problems and building modern, efficient, and scalable applications.My core expertise includes JavaScript, PHP, Laravel, WordPress, MERN Stack, Next.js, RESTful APIs, MySQL, and DatabaseManagementSystems (DBMS).I also have hands-on experience with JavaScript (ES6+), Tailwind CSS, jQuery, and other modern web technologies.I’m passionate about creating impactful digital experiences and contributing to projects that make the web more powerful and accessible.I’m open to exciting job and freelance opportunities that align with my skills and professional growth — while continuously learning and expanding my expertise in AI, Python, and other emerging technologies.",
   email: 'mustafamahmood001@outlook.com',
